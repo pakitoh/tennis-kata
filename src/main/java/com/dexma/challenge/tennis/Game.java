64b206841d58ed4aa1c757cb7c21b005e57e747f
Paco Ramírez <paco.ramirez.romero@gmail.com>
2021-08-10 00:42:52 +0200

    DEV: Second test

--- src/main/java/com/dexma/challenge/tennis/Game.java
+++ src/main/java/com/dexma/challenge/tennis/Game.java
@@ -1,7 +1,10 @@
 package com.dexma.challenge.tennis;
 
 public class Game {
-    public GameScore point(String serverWins, GameScore currentScore) {
+    public GameScore point(String winner, GameScore currentScore) {
+        if("RECEIVER_WINS".equals(winner)) {
+            return new GameScore("15", "30");
+        }
         return new GameScore("15", "0");
     }
 }
--- src/test/java/com/dexma/challenge/tennis/GameTest.java
+++ src/test/java/com/dexma/challenge/tennis/GameTest.java
@@ -10,8 +10,10 @@ import static org.hamcrest.Matchers.equalTo;
 public class GameTest {
 
     public static final String SERVER_WINS = "SERVER_WINS";
+    public static final String RECEIVER_WINS = "RECEIVER_WINS";
     public static final String ZERO = "0";
     public static final String FIFTEEN = "15";
+    public static final String THIRTY = "30";
 
     @Test
     public void shouldReturn15_0WhenScoreIs0_0AndServerWins() {
@@ -19,6 +21,16 @@ public class GameTest {
         GameScore expectedScore = new GameScore(FIFTEEN, ZERO);
 
         GameScore updatedScore = new Game().point(SERVER_WINS, currentScore);
+
+        assertThat(updatedScore, equalTo(expectedScore));
+    }
+
+    @Test
+    public void shouldReturn15_30WhenScoreIs15_15AndReceiverWins() {
+        GameScore currentScore = new GameScore(FIFTEEN, FIFTEEN);
+        GameScore expectedScore = new GameScore(FIFTEEN, THIRTY);
+
+        GameScore updatedScore = new Game().point(RECEIVER_WINS, currentScore);
 
         assertThat(updatedScore, equalTo(expectedScore));
     }
