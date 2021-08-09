45e8ad6ec59c9793172801210f877e7ec5535896
Paco Ramírez <paco.ramirez.romero@gmail.com>
2021-08-10 01:06:38 +0200

    Add test and logic to handle deuce and advantage
    DEV: Fifth test

--- src/main/java/com/dexma/challenge/tennis/Game.java
+++ src/main/java/com/dexma/challenge/tennis/Game.java
@@ -1,8 +1,24 @@
 package com.dexma.challenge.tennis;
 
 public class Game {
+    public static final String SERVER_WINS = "SERVER_WINS";
+    public static final String RECEIVER_WINS = "RECEIVER_WINS";
+    public static final String ZERO = "0";
+    public static final String FIFTEEN = "15";
+    public static final String THIRTY = "30";
+    public static final String FORTY = "40";
+    public static final String ADVANTAGE = "A";
+
     public GameScore point(String winner, GameScore currentScore) {
-        if("RECEIVER_WINS".equals(winner)) {
+        GameScore newScore = calculate(winner, currentScore);
+        if(newScore.server() == ADVANTAGE && newScore.receiver() == ADVANTAGE) {
+            return new GameScore(FORTY, FORTY);
+        }
+        return newScore;
+    }
+
+    private GameScore calculate(String winner, GameScore currentScore) {
+        if ("RECEIVER_WINS".equals(winner)) {
             return new GameScore(currentScore.server(), next(currentScore.receiver()));
         }
         return new GameScore(next(currentScore.server()), currentScore.receiver());
@@ -10,14 +26,14 @@ public class Game {
 
     private String next(String currentScore) {
         switch (currentScore) {
-            case "0":
-                return "15";
-            case "15":
-                return "30";
-            case "30":
-                return "40";
-            case "40":
-                return "A";
+            case ZERO:
+                return FIFTEEN;
+            case FIFTEEN:
+                return THIRTY;
+            case THIRTY:
+                return FORTY;
+            case FORTY:
+                return ADVANTAGE;
         }
         throw new IllegalStateException();
     }
--- src/test/java/com/dexma/challenge/tennis/GameTest.java
+++ src/test/java/com/dexma/challenge/tennis/GameTest.java
@@ -2,20 +2,11 @@ package com.dexma.challenge.tennis;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
+import static com.dexma.challenge.tennis.Game.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class GameTest {
-
-    public static final String SERVER_WINS = "SERVER_WINS";
-    public static final String RECEIVER_WINS = "RECEIVER_WINS";
-    public static final String ZERO = "0";
-    public static final String FIFTEEN = "15";
-    public static final String THIRTY = "30";
-    public static final String FORTY = "40";
-    public static final String ADVANTAGE = "A";
 
     @Test
     public void shouldReturn15_0WhenScoreIs0_0AndServerWins() {
@@ -47,7 +38,6 @@ public class GameTest {
         assertThat(updatedScore, equalTo(expectedScore));
     }
 
-
     @Test
     public void shouldReturn40_AWhenScoreIs40_40AndReceiverWins() {
         GameScore currentScore = new GameScore(FORTY, FORTY);
@@ -57,4 +47,15 @@ public class GameTest {
 
         assertThat(updatedScore, equalTo(expectedScore));
     }
+
+    @Test
+    public void shouldReturn40_40WhenScoreIs40_AAndReceiverWins() {
+        GameScore currentScore = new GameScore(ADVANTAGE, FORTY);
+        GameScore expectedScore = new GameScore(FORTY, FORTY);
+
+        GameScore updatedScore = new Game().point(RECEIVER_WINS, currentScore);
+
+        assertThat(updatedScore, equalTo(expectedScore));
+    }
+
 }
